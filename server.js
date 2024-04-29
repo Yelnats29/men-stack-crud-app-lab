d863d9f9f5235985cfc5f6abf5c9bb1235d6b26d
Yelnats <s.jeanjr@gmail.com>
2024-04-29 14:22:48 -0400

    Completed MVP

--- server.js
+++ server.js
@@ -3,8 +3,12 @@ dotenv.config();
 const express = require("express");
 const mongoose = require("mongoose");
 const Dog = require("./models/dog.js");
+const morgan = require("morgan");
+const methodOverride = require("method-override");
 const app = express();
 app.use(express.urlencoded({ extended: false })); // This creates the req.body.
+app.use(morgan('dev'));
+app.use(methodOverride("_method"));
 mongoose.connect(process.env.MONGODB_URI);
 mongoose.connection.on("connected", () => {
     console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
@@ -31,12 +35,12 @@ app.post('/dogs', async (req, res) => {
         req.body.isGentleAroundPeople = true;
     } else {
         req.body.isGentleAroundPeople = false;
-    } 
+    }
     if (req.body.isGentleAroundAnimals === "on") {
         req.body.isGentleAroundAnimals = true;
     } else {
         req.body.isGentleAroundAnimals = false;
-    } console.log(req.body);  
+    } console.log(req.body);
     await Dog.create(req.body);
     res.redirect('/dogs')
 });
@@ -44,13 +48,41 @@ app.post('/dogs', async (req, res) => {
 // READ - Retrieve All Fruit Data
 app.get("/dogs", async (req, res) => {
     const allDogs = await Dog.find();
-    res.render('allDogs.ejs', {dogs: allDogs});
-  });
+    res.render('allDogs.ejs', { dogs: allDogs });
+});
 
-  // Show Route
-  app.get("/dogs/:dogsId", async (req, res) => {
+// Show Route
+app.get("/dogs/:dogsId", async (req, res) => {
     const foundDog = await Dog.findById(req.params.dogsId)
-    res.render('dogview.ejs', {dog: foundDog})
+    res.render('dogview.ejs', { dog: foundDog })
+});
+
+// Delete
+app.delete("/dogs/:dogsId", async (req, res) => {
+    await Dog.findByIdAndDelete(req.params.dogsId);
+    res.redirect("/dogs")
+});
+
+// Edit
+app.get("/dogs/:dogsId/edit", async (req, res) => {
+    const foundDog = await Dog.findById(req.params.dogsId);
+    res.render('edit.ejs', { dog: foundDog })
+});
+
+// Updates
+app.put("/dogs/:dogsId", async (req, res) => {
+    if (req.body.isGentleAroundPeople === "on") {
+        req.body.isGentleAroundPeople = true;
+    } else {
+        req.body.isGentleAroundPeople = false;
+    }
+    if (req.body.isGentleAroundAnimals === "on") {
+        req.body.isGentleAroundAnimals = true;
+    } else {
+        req.body.isGentleAroundAnimals = false;
+    } console.log(req.body);
+    await Dog.findByIdAndUpdate(req.params.dogsId, req.body);
+    res.redirect('/dogs')
 });
 
 
